2bbf037dc1035c7622650c28aafaccc062cfdcb3
Павел <shmachilin@gmail.com>
2020-05-24 21:43:56 +0300

    Добавлены мульти-конвертеры для объекдинения значений в перечисление и в CompositeCollection

---
+++ MathCore.WPF/Converters/MultiValuesToCompositecollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace MathCore.WPF.Converters
+{
+    [MarkupExtensionReturnType(typeof(MultiValuesToCompositecollection))]
+    public class MultiValuesToCompositecollection : MultiValueValueConverter
+    {
+        protected override object? Convert(object[]? vv, Type? t, object? p, CultureInfo? c)
+        {
+            if (vv is null) return null;
+            var collection = new CompositeCollection();
+            foreach (var value in vv)
+            {
+                var src = value is IEnumerable enumerable 
+                    ? new CollectionContainer { Collection = enumerable } 
+                    : value;
+                collection.Add(src);
+            }
+
+            return collection;
+        }
+    }
+}
---
+++ MathCore.WPF/Converters/MultiValuesToEnumerable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Markup;
+
+namespace MathCore.WPF.Converters
+{
+    [MarkupExtensionReturnType(typeof(MultiValuesToEnumerable))]
+    public class MultiValuesToEnumerable : MultiValueValueConverter
+    {
+        protected override object? Convert(object[]? vv, Type? t, object? p, CultureInfo? c) => vv;
+
+        protected override object[]? ConvertBack(object? v, Type[]? tt, object? p, CultureInfo? c) => 
+            (v as IEnumerable)?.Cast<object>().Zip(tt!, System.Convert.ChangeType).ToArray()!;
+    }
+}
