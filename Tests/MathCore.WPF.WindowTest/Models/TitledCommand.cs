79ac1e5ada85cfdf6bca6975c6578ec370279236
Infarh <shmachilin@yandex.ru>
2020-06-26 22:12:06 +0300

    Тест быстродействия команд

---
+++ Tests/MathCore.WPF.WindowTest/Models/TitledCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Input;
+
+namespace MathCore.WPF.WindowTest.Models
+{
+    class TitledCommand : ICommand
+    {
+        private readonly Action _Execute;
+
+        public string? Title { get; set; }
+
+        public bool CanExecute { get; set; } = true;
+
+        public TitledCommand(Action Execute) => _Execute = Execute;
+        
+        bool ICommand.CanExecute(object parameter)
+        {
+            Debug.WriteLine($"{Title}-{CanExecute}");
+            return CanExecute;
+        }
+
+        void ICommand.Execute(object parameter) => _Execute();
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
---
+++ Tests/MathCore.WPF.WindowTest/TestViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Markup;
+
+using MathCore.WPF.Commands;
+using MathCore.WPF.ViewModels;
+using MathCore.WPF.WindowTest.Models;
+
+namespace MathCore.WPF.WindowTest
+{
+    [MarkupExtensionReturnType(typeof(TestViewModel))]
+    class TestViewModel : ViewModel
+    {
+        private const int __CommandsCount = 100;
+
+        public TitledCommand[] Commands { get; }
+
+        public TestViewModel()
+        {
+            Action command_action = OnCommandExecuted;
+            Commands = Enumerable
+               .Range(1, __CommandsCount)
+               .Select(i => new TitledCommand(command_action) {Title = $"C{i}"})
+               .ToArray();
+        }
+
+        private static readonly Random __Rnd = new Random();
+
+        private void OnCommandExecuted()
+        {
+            for (var i = 0; i < __CommandsCount / 10; i++)
+            {
+                var cmd = Commands[__Rnd.Next(0, __CommandsCount)];
+                cmd.CanExecute ^= true;
+            }
+        }
+    }
+}
