ada14d73a65bcde84c7933230b9d2e2cc8a1ce54
Infarh <shmachilin@yandex.ru>
2021-03-28 23:50:23 +0300

    Базовый класс сервиса диалога с пользвоателем

---
+++ MathCore.WPF/Services/IUserDialogService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathCore.WPF.Services
+{
+    public interface IUserDialogBase
+    {
+        FileInfo? OpenFile(string Title, string Filter = "Все файлы (*.*)|*.*", string? DefaultFilePath = null);
+        FileInfo? SaveFile(string Title, string Filter = "Все файлы (*.*)|*.*", string? DefaultFilePath = null);
+
+        bool YesNoQuestion(string Text, string Title = "Вопрос...");
+        bool OkCancelQuestion(string Text, string Title = "Вопрос...");
+        void Information(string Text, string Title = "Вопрос...");
+        void Warning(string Text, string Title = "Вопрос...");
+        void Error(string Text, string Title = "Вопрос...");
+    }
+}
