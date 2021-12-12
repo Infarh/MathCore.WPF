e823b386d1c0f17d35cc8f2dd13bc3f456a9e6f7
Infarh <shmachilin@yandex.ru>
2021-12-12 17:02:50 +0300

    0.0.28 - Обновление платформы и пакетов

--- Tests/MathCore.WPF.WindowTest/ViewModels/PolynomProjectionViewModel.cs
+++ Tests/MathCore.WPF.WindowTest/ViewModels/PolynomProjectionViewModel.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows.Markup;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using XPlot.Plotly;
 
 using MathCore.WPF.ViewModels;
 
@@ -7,7 +13,53 @@ namespace MathCore.WPF.WindowTest.ViewModels;
 [MarkupExtensionReturnType(typeof(PolynomProjectionViewModel))]
 public class PolynomProjectionViewModel : TitledViewModel
 {
-    public PolynomProjectionViewModel() => Title = "Проверка";
+    public PolynomProjectionViewModel()
+    {
+        Title = "Проверка";
+        _Points = new()
+        {
+            new(0,0),
+            new(1, 65535)
+        };
+
+        var x = Enumerable.Range(1, 10);
+        var y = Enumerable.Range(1, 10);
+
+        var chart = Chart.Plot(
+            new Scatter
+            {
+                x = x,
+                y = y,
+                mode = "lines+markers"
+            }
+        );
+
+        var chart1_layout = new Layout.Layout
+        {
+            title = "Open Price",
+            xaxis = new Xaxis
+            {
+                title = "Date"
+            },
+            yaxis = new Yaxis
+            {
+                title = "Price (USD)"
+            }
+        };
+        chart.WithLayout(chart1_layout);
+        var html = chart.GetHtml();
+        var web_browser = new WebBrowser{ Source = new("http://yandex.ru")};
+        var window = new Window
+        {
+            Content = web_browser
+        };
+        window.Loaded += (_, _) => web_browser.NavigateToString(html);
+        window.Show();
+    }
+
+    private ObservableCollection<Point> _Points;
+
+    public IEnumerable<Point> Points => _Points;
 
 
 }
