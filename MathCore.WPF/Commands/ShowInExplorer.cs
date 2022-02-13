bbd70234c9988fbbcf0c9c8e0870145acbbd3e8e
Infarh <shmachilin@yandex.ru>
2022-02-13 15:41:03 +0300

    0.0.32.2 - Обновление пакетов

--- MathCore.WPF/Commands/ShowInExplorer.cs
+++ MathCore.WPF/Commands/ShowInExplorer.cs
@@ -12,8 +12,8 @@ namespace MathCore.WPF.Commands
         public override bool CanExecute(object? parameter) => parameter switch
         {
             string path when File.Exists(path) || Directory.Exists(path) => true,
-            FileSystemInfo {Exists: true} => true,
-            DriveInfo {IsReady: true} => true,
+            FileSystemInfo { Exists: true } => true,
+            DriveInfo { IsReady: true } => true,
             _ => false
         };
 
@@ -30,10 +30,10 @@ namespace MathCore.WPF.Commands
                     case string path when Directory.Exists(path):
                         parameter = new DirectoryInfo(path);
                         continue;
-                    case FileSystemInfo {Exists: true} file_or_dir:
-                        file_or_dir.ShowInFileExplorer();
+                    case FileSystemInfo { Exists: true } file_or_dir:
+                        file_or_dir.ShowInExplorer();
                         break;
-                    case DriveInfo {IsReady: true} drive:
+                    case DriveInfo { IsReady: true } drive:
                         parameter = drive.RootDirectory;
                         continue;
                 }
