b79be4c6aaf239d7e8eff2937feb10fd7b7e36b9
Infarh <shmachilin@yandex.ru>
2022-06-10 12:51:56 +0300

    0.0.34.1 - Исправление работы динамической модели - трансляция контекста данных

---
+++ MathCore.WPF/Triggers/DebugActionTrigger.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+using Microsoft.Xaml.Behaviors;
+
+namespace MathCore.WPF.Triggers;
+
+public class DebugActionTrigger : TriggerAction<DependencyObject>
+{
+    public static event Action<string, object, DependencyObject, object> TriggerEvent;
+
+    #region Message : string - Сообщение
+
+    /// <summary>Сообщение</summary>
+    //[Category("")]
+    [Description("Сообщение")]
+    public string Message
+    {
+        get => (string)GetValue(MessageProperty);
+        set => SetValue(MessageProperty, value);
+    }
+
+    /// <summary>Сообщение</summary>
+    public static readonly DependencyProperty MessageProperty =
+        DependencyProperty.Register(
+            nameof(Message),
+            typeof(string),
+            typeof(DebugActionTrigger),
+            new PropertyMetadata(default(string)));
+
+    #endregion
+
+    #region MessageParameter : object - Параметр сообщения
+
+    /// <summary>Параметр сообщения</summary>
+    //[Category("")]
+    [Description("Параметр сообщения")]
+    public object MessageParameter
+    {
+        get => (object)GetValue(MessageParameterProperty);
+        set => SetValue(MessageParameterProperty, value);
+    }
+
+    /// <summary>Параметр сообщения</summary>
+    public static readonly DependencyProperty MessageParameterProperty =
+        DependencyProperty.Register(
+            nameof(MessageParameter),
+            typeof(object),
+            typeof(DebugActionTrigger),
+            new PropertyMetadata(default));
+
+    #endregion
+
+    protected override void Invoke(object parameter) => TriggerEvent?.Invoke(Message, MessageParameter, AssociatedObject, parameter);
+}
---
+++ Tests/MathCore.WPF.WindowTest/Infrastructure/Converters/DebugConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Markup;
+
+using MathCore.WPF.Converters.Base;
+
+namespace MathCore.WPF.WindowTest.Infrastructure.Converters;
+
+[MarkupExtensionReturnType(typeof(DebugConverter))]
+public class DebugConverter : ValueConverter
+{
+    public bool Enabled { get; set; } = true;
+
+    protected override object? Convert(object? v, Type t, object? p, CultureInfo c)
+    {
+        if(Enabled)
+            Debugger.Break();
+        return v;
+    }
+}
