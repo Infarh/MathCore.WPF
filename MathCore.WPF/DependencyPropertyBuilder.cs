9c21ceab19f074c2f63a776d06a7c9a245d07d52
Infarh <shmachilin@yandex.ru>
2022-06-11 19:36:38 +0300

    0.0.34.3 - Исправление проблем

---
+++ MathCore.WPF/DependencyPropertyBuilder.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq.Expressions;
+using System.Windows;
+
+namespace MathCore.WPF;
+
+public static class DependencyPropertyBuilder
+{
+    public static DependencyPropertyClassBuilder<T> Register<T>() where T : DependencyObject => new();
+}
+
+public readonly ref struct DependencyPropertyClassBuilder<T> where T : DependencyObject
+{
+    public DependencyPropertyBuilder<T, TValue> Property<TValue>(Expression<Func<T, TValue>> PropertySelector)
+    {
+        if (PropertySelector is not { Body: MemberExpression { Member: { Name: var name } } })
+            throw new InvalidOperationException("Селектор должен выбирать свойство объекта");
+        return new(name);
+    }
+}
+
+public readonly ref struct DependencyPropertyBuilder<T, TValue> where T : DependencyObject
+{
+    public delegate void PropertyChangedHandler(T Sender, TValue? OldValue, TValue? NewValue);
+
+    public string Name { get; init; }
+
+    public PropertyMetadata? Metadata { get; init; } = new();
+
+    public TValue? DefaultValue { get; init; } = default;
+
+    public PropertyChangedCallback? PropertyChangedCallback { get; init; } = null;
+    public CoerceValueCallback? CoerceValueCallback { get; init; } = null;
+
+    public DependencyPropertyBuilder(string Name) => this.Name = Name;
+
+    public DependencyPropertyBuilder<T, TValue> WithDefaultValue(TValue value) => this with { DefaultValue = value };
+
+    public DependencyPropertyBuilder<T, TValue> OnChanged(PropertyChangedCallback? ChangedCallback) =>
+        ChangedCallback is null ? this with { PropertyChangedCallback = null }
+            : PropertyChangedCallback is null
+                ? this with { PropertyChangedCallback = ChangedCallback }
+                : this with { PropertyChangedCallback = PropertyChangedCallback + ChangedCallback };
+
+    public DependencyPropertyBuilder<T, TValue> OnChanged(PropertyChangedHandler? ChangedCallback) =>
+        ChangedCallback is null ? this with { PropertyChangedCallback = null } 
+            : PropertyChangedCallback is null
+                ? this with { PropertyChangedCallback = (d, e) => ChangedCallback((T)d, (TValue?)e.OldValue, (TValue?)e.NewValue) }
+                : this with { PropertyChangedCallback = PropertyChangedCallback + ((d, e) => ChangedCallback((T)d, (TValue?)e.OldValue, (TValue?)e.NewValue)) };
+
+    public DependencyPropertyBuilder<T, TValue> CoerceValue(CoerceValueCallback? Coerce) => this with { CoerceValueCallback = Coerce };
+
+    public DependencyProperty Build()
+    {
+        Metadata.DefaultValue = DefaultValue;
+        Metadata.PropertyChangedCallback = PropertyChangedCallback;
+        Metadata.CoerceValueCallback = CoerceValueCallback;
+
+        switch (Metadata)
+        {
+            case FrameworkPropertyMetadata framework_metadata:
+
+                break;
+
+            case UIPropertyMetadata ui_metadata:
+                //ui_metadata.
+                break;
+        }
+
+        var property = DependencyProperty
+           .Register(
+                Name,
+                typeof(T),
+                typeof(TValue),
+                Metadata);
+
+        //PropertyMetadata p = new PropertyMetadata(DefaultValue, );
+        //p.DefaultValue = null;
+
+        return property;
+    }
+}
--- Tests/MathCore.WPF.WindowTest/Program.cs
+++ Tests/MathCore.WPF.WindowTest/Program.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
-namespace MathCore.WPF.WindowTest
+using OxyPlot.Series;
+
+namespace MathCore.WPF.WindowTest;
+
+internal static class Program
 {
-    internal static class Program
+    [STAThread]
+    public static void Main(string[] Args)
     {
-        [STAThread]
-        public static void Main(string[] Args)
-        {
-            var app = new App();
-            app.InitializeComponent();
-            app.Run();
-        }
+        var property = DependencyPropertyBuilder
+           .Register<TestControl>()
+           .Property(p => p.Title)
+           .OnChanged((control, OldValue, NewValue) => { });
+
+        var app = new App();
+        app.InitializeComponent();
+        app.Run();
     }
+}
+
+internal class TestControl : Control
+{
+    public string Title { get; set; }
 }
