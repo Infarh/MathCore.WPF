817521d9e2769c91b8e23b558f9296cb6beb04cc
Infarh <shmachilin@yandex.ru>
2022-09-18 21:21:43 +0300

    0.0.37 - Обновление пакетов

---
+++ MathCore.WPF/Extensions/ValueConverterEx.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Data;
+
+namespace MathCore.WPF.Extensions;
+
+public static class ValueConverterExtensions
+{
+    public static IEnumerable<(Type SourceType, Type TargetType)> GetTypeConversion(this IValueConverter converter)
+    {
+#if NET6_0_OR_GREATER
+        ArgumentNullException.ThrowIfNull(converter);
+#else
+        if (converter is null) throw new ArgumentNullException(nameof(converter));
+#endif
+
+        var any = false;
+        foreach (var attribute in converter.NotNull().GetType().GetCustomAttributes<ValueConversionAttribute>())
+        {
+            if (!any) any = true;
+            yield return (attribute.SourceType, attribute.TargetType);
+        }
+
+        if (!any)
+            yield return (typeof(object), typeof(object));
+    }
+}
--- MathCore.WPF/NestedBinding.cs
+++ MathCore.WPF/NestedBinding.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows.Markup;
 
@@ -12,6 +7,7 @@ namespace MathCore.WPF;
 
 [Copyright("adeptuss", url = "https://habr.com/ru/post/277157/")]
 [ContentProperty(nameof(Bindings))]
+[MarkupExtensionReturnType(typeof(object))]
 public class NestedBinding : MarkupExtension
 {
     public NestedBinding() => Bindings = new Collection<BindingBase>();
