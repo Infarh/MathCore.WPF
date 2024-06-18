b541cc97f33163b85878ebc8940fb778f7b1f5f9
Павел <shmachilin@gmail.com>
2024-06-18 20:46:49 +0300

    Ручной режим обновления привязки в TextBox по нажанию Enter

---
+++ MathCore.WPF/Extensions/DependencyObjectEx.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace MathCore.WPF.Extensions;
+
+public static class DependencyObjectEx
+{
+    public static Binding? BindingGet(this DependencyObject obj, DependencyProperty property) => BindingOperations
+        .GetBinding(obj.NotNull(), property.NotNull());
+
+    public static BindingBase? BindingGetBase(this DependencyObject obj, DependencyProperty property) => BindingOperations
+        .GetBindingBase(obj.NotNull(), property.NotNull());
+
+    public static BindingExpression? BindingGetExpression(this DependencyObject obj, DependencyProperty property) => BindingOperations
+        .GetBindingExpression(obj.NotNull(), property.NotNull());
+
+    public static BindingExpressionBase? BindingGetExpressionBase(this DependencyObject obj, DependencyProperty property) => BindingOperations
+        .GetBindingExpressionBase(obj.NotNull(), property.NotNull());
+
+    public static void BindingClearAll(this DependencyObject obj) => BindingOperations.ClearAllBindings(obj.NotNull());
+
+    public static void BindingClear(this DependencyObject obj, DependencyProperty property) => BindingOperations
+        .ClearBinding(obj.NotNull(), property.NotNull());
+
+    public static void BindingUpdateSource(this DependencyObject obj, DependencyProperty property) => obj
+        .BindingGetExpression(property)?
+        .UpdateSource();
+
+    public static void BindingUpdateTarget(this DependencyObject obj, DependencyProperty property) => obj
+        .BindingGetExpression(property)?
+        .UpdateTarget();
+
+    public static void BindingValidateWithoutUpdate(this DependencyObject obj, DependencyProperty property) => obj
+        .BindingGetExpression(property)?
+        .ValidateWithoutUpdate();
+}
---
+++ MathCore.WPF/TextBoxEx.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace MathCore.WPF;
+
+public static class TextBoxEx
+{
+    #region Attached property UpdateBindingSourceOnEnter : bool - Обновить привязку при нажатии Enter
+
+    /// <summary>Обновить привязку при нажатии Enter</summary>
+    public static readonly DependencyProperty UpdateBindingSourceOnEnterProperty =
+        DependencyProperty.RegisterAttached(
+            "UpdateBindingSourceOnEnter",
+            typeof(bool),
+            typeof(TextBoxEx),
+            new(default(bool), OnUpdateBindingOnEnterChanged));
+
+    private static void OnUpdateBindingOnEnterChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+    {
+        if(sender is not TextBox text_box || e.NewValue is not bool value) return;
+
+        if (value)
+            text_box.KeyDown += OnTextBoxKeyDown;
+        else
+            text_box.KeyDown -= OnTextBoxKeyDown;
+    }
+
+    private static void OnTextBoxKeyDown(object Sender, KeyEventArgs E)
+    {
+        if(E.Key != Key.Enter) return;
+
+        var text_box = (TextBox)Sender;
+
+        if (text_box.AcceptsReturn && !E.KeyboardDevice.IsKeyDown(Key.LeftCtrl)) 
+            return;
+
+        if (BindingOperations.GetBindingExpression(text_box, TextBox.TextProperty) is not { } binding) 
+            return;
+
+        if(binding.ParentBinding.UpdateSourceTrigger != UpdateSourceTrigger.Explicit && !E.KeyboardDevice.IsKeyDown(Key.LeftCtrl))
+            return;
+
+        binding.UpdateSource();
+        E.Handled = true;
+    }
+
+    /// <summary>Обновить привязку при нажатии Enter</summary>
+    [AttachedPropertyBrowsableForType(typeof(TextBox))]
+    public static void SetUpdateBindingSourceOnEnter(DependencyObject d, bool value) => d.SetValue(UpdateBindingSourceOnEnterProperty, value);
+
+    /// <summary>Обновить привязку при нажатии Enter</summary>
+    public static bool GetUpdateBindingSourceOnEnter(DependencyObject d) => (bool)d.GetValue(UpdateBindingSourceOnEnterProperty);
+
+    #endregion
+}
