eb7eb33c04d9f36b4381e3b8cd2cec0a949ca672
Infarh <shmachilin@gmail.com>
2025-03-07 08:40:22 +0300

    Добавлены команды для коммита и отката изменений
    В файле `BindingGroupCommit.cs` реализован класс `BindingGroupCommit`, который позволяет коммитить изменения в объекте `BindingGroup`, если он помечен как "грязный" (IsDirty).
    
    В файле `BindingGroupRollback.cs` добавлен класс `BindingGroupRollback`, который отменяет изменения в объекте `BindingGroup`, также проверяя его состояние "грязного" изменения.

---
+++ MathCore.WPF/Commands/BindingGroupRollback.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Data;
+
+namespace MathCore.WPF.Commands;
+
+/// <summary>Команда для отмены изменений в BindingGroup, если они были изменены</summary>
+public class BindingGroupRollback : Command
+{
+    public override bool CanExecute(object? parameter) => parameter is BindingGroup { IsDirty: true };
+
+    public override void Execute(object? parameter) => (parameter as BindingGroup).CancelEdit();
+}
