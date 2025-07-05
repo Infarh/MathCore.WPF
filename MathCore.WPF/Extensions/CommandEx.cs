ec0a975e8fd4fbe3b73adef3ac819a9b10ec059e
Infarh <shmachilin@gmail.com>
2025-07-05 15:01:22 +0300

    Рефакторинг класса метдов-расширений интерфейса команды ICommand

---
+++ MathCore.WPF/Extensions/CommandEx.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Input;
+
+using MathCore.WPF.Commands;
+
+namespace MathCore.WPF.Extensions;
+
+/// <summary>Статический класс-расширение для работы с командами WPF</summary>
+public static class CommandEx
+{
+    /// <summary>Устанавливает имя и описание для команды</summary>
+    /// <typeparam name="TCommand">Тип команды</typeparam>
+    /// <param name="Command">Команда</param>
+    /// <param name="Name">Имя команды</param>
+    /// <param name="Description">Описание команды</param>
+    /// <returns>Команда с установленными именем и описанием</returns>
+    public static TCommand WithName<TCommand>(this TCommand Command, string Name, string? Description = null)
+        where TCommand : Command
+    {
+        Command.Name = Name;
+        if (Description is not null)
+            Command.Description = Description;
+        return Command;
+    }
+
+    /// <summary>Пытается выполнить команду с указанным параметром</summary>
+    /// <param name="Command">Команда</param>
+    /// <param name="Parameter">Параметр команды</param>
+    /// <returns>Истина, если команда была успешно выполнена</returns>
+    public static bool TryExecute(this ICommand? Command, object Parameter)
+    {
+        if (Command is null || !Command.CanExecute(Parameter)) return false;
+
+        Command.Execute(Parameter);
+
+        return true;
+    }
+}
