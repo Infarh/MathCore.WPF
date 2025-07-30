154a70cf637c2868bfb36b49ce227e905a74f7c2
Infarh <shmachilin@gmail.com>
2025-07-30 21:38:05 +0300

    Обновление окон и моделей представления
    В файле `TestWindow7.xaml` заменён элемент `Ellipse` с изменёнными атрибутами.
    Добавлен новый класс окна `TestWindows8` в `TestWindows8.xaml` с необходимыми пространствами имён и ресурсами.
    В `TestWindows8.xaml.cs` реализована инициализация компонентов для нового окна.
    Создан класс `TestWindow8ViewModel` в `TestWindow8ViewModel.cs`, наследующий от `TitledViewModel`, с установленным заголовком окна.

---
+++ Tests/MathCore.WPF.WindowTest/ViewModels/TestWindow8ViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Markup;
+
+using MathCore.WPF.ViewModels;
+
+namespace MathCore.WPF.WindowTest.ViewModels;
+
+[MarkupExtensionReturnType(typeof(TestWindow8ViewModel))]
+internal class TestWindow8ViewModel() : TitledViewModel("Тестовое окно 8")
+{
+
+}
