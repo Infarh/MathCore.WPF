f6ece760b541e69f39a8fbef756e0e4d8c969c9a
Infarh <shmachilin@gmail.com>
2026-02-02 23:17:11 +0300

    Добавлен класс TerminateApp для принудительного выхода
    Добавлен новый класс TerminateApp в MathCore.WPF.Commands, наследующийся от CloseApp. Класс реализует команду завершения приложения с возможностью указания кода выхода через параметр команды. Если код не задан, используется 0.

---
+++ MathCore.WPF/Commands/TerminateApp.cs
@@ -0,0 +1,20 @@
+﻿namespace MathCore.WPF.Commands;
+
+/// <summary>Команда для завершения всего приложения принудительно</summary>
+public class TerminateApp : CloseApp
+{
+    /// <summary>Выполняет команду завершения всего приложения</summary>
+    public override void Execute(object? p)
+    {
+        var code = p switch
+        {
+            int c => c,
+            string s when int.TryParse(s, out var v) => v,
+            _ => ExitCode
+        };
+        if (code is not null)
+            Environment.Exit(code.Value);
+        else
+            Environment.Exit(0);
+    }
+}
